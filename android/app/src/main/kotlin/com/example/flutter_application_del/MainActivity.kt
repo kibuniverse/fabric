5c6d59c225e916865bed48c7546a5d8aa2df8764
unikz <3050540005@qq.com>
2024-10-27 21:16:54 +0800

    feat: init

---
+++ android/app/src/main/kotlin/com/example/flutter_application_del/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_del
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data-types/index.dart
@@ -0,0 +1,7 @@
+class CounterModel {
+  /// 是否开启震动
+  bool enableVibration = true;
+
+  /// 是否开启屏幕常亮
+  bool enableKeepScreenOn = true;
+}
